552b8984bce19b3d5947d2030669eb5f3676fda3
midolife20 <rahalhamido@gmail.com>
2024-07-03 12:51:57 +0100

    adding some text

--- pages/_document.js
+++ pages/_document.js
@@ -23,10 +23,16 @@ class MyDocument extends Document {
             href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
+          
+          {/* Additional meta tags or other headers can go here */}
+          <meta name="description" content="YouTube Thumbnail Extractor - Get free thumbnail image of any YouTube videos in Full HD, HD, SD, and small sizes." />
+          <title>YouTube Thumbnail Extractor</title>
         </Head>
         <body>
           <Main />
           <NextScript />
+          
+          {/* Script or other elements specific to your document can be added here */}
         </body>
       </Html>
     );
